8fb412a534af7bc5e7a87c7e21c234037f2a3081
Dev <dev@spabloshi.com>
2020-07-15 17:33:51 +0100

    get the phone app creating and starting

--- bin/create_reason_react_native_app.sh
+++ bin/create_reason_react_native_app.sh
@@ -1,3 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
 npm install -g react-native-cli
 npx @react-native-community/cli init --template @reason-react-native/template $name;format="Camel"$App
 mv $name;format="Camel"$App app
+cd app
+yarn install
+yarn re:build
+#yarn start --reset-cache
+npx react-native run-ios
