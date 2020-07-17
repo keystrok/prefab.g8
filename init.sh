d1715a6f85e5c69cf2c91d7c15e10d6f6a320ad3
Dev <dev@spabloshi.com>
2020-07-17 13:56:42 +0100

    add script for starting the phone

--- init.sh
+++ init.sh
@@ -10,7 +10,8 @@ install_reason_react_native () {
   npm install -g react-native-cli
   npx @react-native-community/cli init --template @reason-react-native/template $name;format="Camel"$App
   mv $name;format="Camel"$App app
-  mv templates/reasonml_tea/* app/src
+  cp templates/reason_react_native/*.sh app/
+  cp templates/reason_react_native/src/* app/src/
 }
 
 $if(reason-react-native.truthy)$
