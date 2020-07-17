9ab30db31d181824371dbf3ba26a6ae2eaeb59ed
Dev <dev@spabloshi.com>
2020-07-17 13:37:08 +0100

    seperate the init and running bits

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@
 set -e
 
 leave_no_trace () {
+   rm -rf templates/
    rm -- "\${0}"
 }
 
@@ -9,20 +10,11 @@ install_reason_react_native () {
   npm install -g react-native-cli
   npx @react-native-community/cli init --template @reason-react-native/template $name;format="Camel"$App
   mv $name;format="Camel"$App app
-  mv reasonml_tea/* app/src
-  rm -rf reasonml_tea/
+  mv templates/reasonml_tea/* app/src
 }
 
 $if(reason-react-native.truthy)$
-  install_reason_react_native
-
-  #TIP: should probably be in a runClient.sh or something
-  cd app
-  yarn install
-  yarn re:build
-  #yarn start --reset-cache
-  npx react-native run-ios
-
+install_reason_react_native
 $endif$
 
 leave_no_trace
